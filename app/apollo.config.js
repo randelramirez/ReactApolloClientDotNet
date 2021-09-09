be6ad286e79ee6a283c30a03b2fd895c1b81ac6b
Randel Ramirez <randelramirez1@gmail.com>
2021-09-09 16:30:03 +0800

    updated and refactor

---
+++ app/apollo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  client: {
+    service: {
+      //an alternative is, local copy can be downloaded using localSchemaFile
+      url: "http://localhost:4000/graphql",
+      skipSSLValidation: true,
+    }
+  }
+}
