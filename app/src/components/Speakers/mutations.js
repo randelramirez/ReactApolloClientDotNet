efe00c48dc0e39f92af18cc17699fe224dcc8e62
Randel Ramirez <randelramirez1@gmail.com>
2021-09-10 23:46:44 +0800

    Add comments

--- app/src/components/Speakers/mutations.js
+++ app/src/components/Speakers/mutations.js
@@ -1,5 +1,6 @@
 import { gql } from "@apollo/client";
 
+// we return the featured property as well so that when the featured is updated by this mutation the cache will be updated as with new value of featured
 export const FEATURED_SPEAKER = gql`
   mutation markFeatured($speakerId: ID!, $featured: Boolean!) {
     markFeatured(speakerId: $speakerId, featured: $featured) {
