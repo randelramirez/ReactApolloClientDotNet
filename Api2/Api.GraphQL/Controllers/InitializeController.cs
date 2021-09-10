0886bf90d16b3b6f64bcf0062642ad636346155f
Randel Ramirez <randelramirez1@gmail.com>
2021-09-10 22:45:29 +0800

    Code clean up

--- Api2/Api.GraphQL/Controllers/InitializeController.cs
+++ Api2/Api.GraphQL/Controllers/InitializeController.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Api.GraphQL.Models;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 
 namespace Api.GraphQL.Controllers
 {
+    // TO DO: DELETE THIS CONTROLLER LATER
     [Route("api/[controller]")]
     [ApiController]
     public class InitializeController : ControllerBase
